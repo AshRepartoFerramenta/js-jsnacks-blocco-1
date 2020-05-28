3e4c23a52e7200eca6c0e96ad37b3a2b271342df
AshRepartoFerramenta <dcapod07@hotmail.com>
2020-05-28 18:14:44 +0200

    Quinto Blocco non funziona( errore 'isNan')

--- js/script.js
+++ js/script.js
@@ -1,91 +1,111 @@
-// Snack1
-// L’utente inserisce due numeri in successione, con due
-// prompt. Il software stampa il maggiore.
+// // Snack1
+// // L’utente inserisce due numeri in successione, con due
+// // prompt. Il software stampa il maggiore.
+//
+// var numero1 = parseInt(prompt('Inserisci il primo numero'));
+// var numero2 = parseInt(prompt('Inserisci il secondo numero'));
+//
+//     //Condizione di numero 1 maggiore di numero 2
+// if (numero1 > numero2) {
+//   console.log(numero1);
+//     //Condizione di numero 2 maggiore di numero 1
+// } else if (numero1 < numero2) {
+//   console.log(numero2);
+//   // Altrimenti
+// } else {
+//   console.log('I numeri scelti sono uguali!');
+// }
+//
+//
+// //Snack2
+// //L’utente inserisce due parole in successione, con due
+// //prompt. Il software stampa prima la parola più corta, poi la
+// //parola più lunga.
+//
+// // Inserire la prima parola scelta
+// var nome1 = prompt('Inserisci la prima parola che hai scelto');
+// // Inserire la seconda parola scelta
+// var nome2 = prompt('Inserisci la seconda parola che hai scelto');
+//
+//
+//
+//     //Condizione di maggioranza del primo nome
+// if (nome1.length > nome2.length) {
+//     //Condizione di maggioranza del secondo nome
+// } else if (nome1.length < nome2.length) {
+//   console.log(nome1, nome2);
+//     //In caso contrario
+// } else {
+//   console.log('Le due parole hanno la stessa lunghezza');
+//  }
+//
+//  // Snack3
+// // Il software deve chiedere per 5 volte all’utente di inserire un
+// // numero. Il programma stampa la somma di tutti i numeri
+// // inseriti. Esegui questo programma in due versioni, con il for e while
+//
+//
+// // Versione con 'for'
+// var somma = 0;
+// for (var i = 0; i < 5; i++) {
+//   var numeri = parseInt(prompt('Inserisci un numero'));
+//   somma += numeri;
+// }
+// console.log(somma);
+//
+//
+// // Versione con 'while'
+// var somma = 0;
+// var i = 0;
+//
+// while (i < 5) {
+//   var numeri = parseInt(prompt('Inserisci un numero'));
+//   somma += numeri;
+//   i++;
+// }
+// console.log(somma);
+//
+//
+//  // Snack4
+// //In un array sono contenuti i nomi degli invitati alla festa del grande
+// //squadrone che ha vinto i mondiali di calcio nel 2006,
+// // chiedi all’utente il suo cognome e comunicagli se può partecipare o no alla festa.
+//
+// //Inserire la lista dei campioni del mondo invitati al Gran Galà
+// var listaInvitati = ['Buffon', 'Cannavaro', 'Nesta', 'Barzagli', 'Grosso', 'Pirlo', 'Del Piero', 'Gattuso', 'Totti', 'Materazzi', 'Toni', 'Oddo', 'Zaccardo','De Rossi','Zidane'];
+// var nomeUtente = prompt('Qual è il tuo cognome?');
+//
+//
+// //Inserire la variabile che indica il messaggio di non entrata
+// var messaggio = 'Pussa via Crucco!';
+// var i = 0;
+//
+// //Inserire le condizioni di entrata alla festa
+// while (i < listaInvitati.length) {
+//   var nomeCorrente = listaInvitati[i];
+//   if (nomeUtente == nomeCorrente) {
+//       //Inserire il messaggio di avvenuta vittoria e di entrata al party
+//     messaggio = 'Campioni del mondo,campioni del mondo,campioni del mondo!';
+//   }
+//   i++;
+// }
 
-var numero1 = parseInt(prompt('Inserisci il primo numero'));
-var numero2 = parseInt(prompt('Inserisci il secondo numero'));
+//Snake 4
+//Chiedi un numero di 4 cifre all’utente e calcola la somma
+//di tutte le cifre che compongono il numero.
 
-    //Condizione di numero 1 maggiore di numero 2
-if (numero1 > numero2) {
-  console.log(numero1);
-    //Condizione di numero 2 maggiore di numero 1
-} else if (numero1 < numero2) {
-  console.log(numero2);
-  // Altrimenti
-} else {
-  console.log('I numeri scelti sono uguali!');
+//Inserire il messaggio iniziale
+var numero=prompt('Inserire un numero di 4 cifre');
+//Inserire la condizione di calcolo
+while ((isNan(numero)) || (numero.length !=4)) {      // errore di riconoscimento 'isNaN'
+    numero = prompt('Inserire un numero di 4 cifre');
 }
+// Variabile che definisce la condizione di salto del numero
+var numeroChiesto= numero.split('');
 
-
-//Snack2
-//L’utente inserisce due parole in successione, con due
-//prompt. Il software stampa prima la parola più corta, poi la
-//parola più lunga.
-
-// Inserire la prima parola scelta
-var nome1 = prompt('Inserisci la prima parola che hai scelto');
-// Inserire la seconda parola scelta
-var nome2 = prompt('Inserisci la seconda parola che hai scelto');
-
-
-
-    //Condizione di maggioranza del primo nome
-if (nome1.length > nome2.length) {
-    //Condizione di maggioranza del secondo nome
-} else if (nome1.length < nome2.length) {
-  console.log(nome1, nome2);
-    //In caso contrario
-} else {
-  console.log('Le due parole hanno la stessa lunghezza');
- }
-
- // Snack3
-// Il software deve chiedere per 5 volte all’utente di inserire un
-// numero. Il programma stampa la somma di tutti i numeri
-// inseriti. Esegui questo programma in due versioni, con il for e while
-
-
-// Versione con 'for'
+//esecuzione della somma
 var somma = 0;
-for (var i = 0; i < 5; i++) {
-  var numeri = parseInt(prompt('Inserisci un numero'));
-  somma += numeri;
+for (var i = 0; 1 < numeroChiesto.length; i++) {
+    var somma = somma + parseInt(numeroChiesto[i]);
 }
 console.log(somma);
-
-
-// Versione con 'while'
-var somma = 0;
-var i = 0;
-
-while (i < 5) {
-  var numeri = parseInt(prompt('Inserisci un numero'));
-  somma += numeri;
-  i++;
-}
-console.log(somma);
-
-
- // Snack4
-//In un array sono contenuti i nomi degli invitati alla festa del grande
-//squadrone che ha vinto i mondiali di calcio nel 2006,
-// chiedi all’utente il suo cognome e comunicagli se può partecipare o no alla festa.
-
-//Inserire la lista dei campioni del mondo invitati al Gran Galà
-var listaInvitati = ['Buffon', 'Cannavaro', 'Nesta', 'Barzagli', 'Grosso', 'Pirlo', 'Del Piero', 'Gattuso', 'Totti', 'Materazzi', 'Toni', 'Oddo', 'Zaccardo','De Rossi','Zidane'];
-var nomeUtente = prompt('Qual è il tuo cognome?');
-
-
-//Inserire la variabile che indica il messaggio di non entrata
-var messaggio = 'Pussa via Crucco!';
-var i = 0;
-
-//Inserire le condizioni di entrata alla festa
-while (i < listaInvitati.length) {
-  var nomeCorrente = listaInvitati[i];
-  if (nomeUtente == nomeCorrente) {
-      //Inserire il messaggio di avvenuta vittoria e di entrata al party
-    messaggio = 'Campioni del mondo,campioni del mondo,campioni del mondo!';
-  }
-  i++;
-}
