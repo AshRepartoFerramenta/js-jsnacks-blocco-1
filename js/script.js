aa8c3c192380923567e8432be5cbc2838808bb4f
AshRepartoFerramenta <dcapod07@hotmail.com>
2020-06-05 17:54:19 +0200

    add new jsnak

--- js/script.js
+++ js/script.js
@@ -140,3 +140,21 @@ for (var i = 0; i < falseList; i++) {
 
 //risultato generato random
 falseList.push(risultatoFalso);
+
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+//fino a quando ne avrà tanti quanti l’altro.
+
+
+// Inserisco il numero di elementi nel prima array
+var array1 = ['quiquoqua', 'junior', 'drago shenron', 're kaio', 'crillin', 'goku', 'vegeta', 'c-18', 'c-16', 'c-17'];
+//Inserisco il numero di elementi nel secondo array
+var array2 = ['gogeta', 'vegetto', 'trunks', 'goten'];
+var casualItem = ['majinbu', 'bulma', 'gohan', 'cell', 'broly', 'songoku', 'freezer'];
+
+// eseguo la condizione random di elementi generati
+while(array1.length > array2.length) {
+  array2.push(casualItem[Math.floor(Math.random()*casualItem.length)]);
+}
+console.log(array1);
+console.log(array2);
